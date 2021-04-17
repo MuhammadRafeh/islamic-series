4c0625188c4958df6dda6dcca691791aeae12687
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-04-17 15:00:17 +0300

    Added seljhuk episodes

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -21,3 +21,89 @@ const jsCode = `
   document.getElementsByClassName("ytp-youtube-button ytp-button yt-uix-sessionlink")[0].remove();
   document.getElementsByClassName("annotation annotation-type-custom iv-branding")[0].remove();
   `;
+
+  //The great saljhuk
+  // episode 1 2188262094639710
+  // episode 2 109296617761719
+  // episode 3 111878720836842
+  // episode 4 145536813962375
+  // episode 5 210764103812507
+  // episode 6 212632726958978
+  // episode 7 214920950063489
+  // episode 8 216610516561199
+  // episode 9 221958982693019
+  // episode 10 226672985554952
+  // episode 11 231905868364997
+  // episode 12 236915077864076
+  // episode 13 197451365358318
+  // episode 14 201796761590445
+  // episode 15 206485954454859
+  // episode 16 259898562232394
+  // episode 17 265421855013398
+  // episode 18 220089243094530
+  // episode 19 224571342646320
+  // episode 20 229188968851224
+  // episode 21 283711949851055
+  // episode 22 239135201189934
+  // episode 23 243687247401396
+  // episode 24 130832372253162
+  // episode 25 253501969753257
+  // episode 26 263266798776774
+  // episode 27 316146903274226
+  // episode 28 
+
+//   The great saljuk all episodes Facebook links
+// Episode 1 
+// https://m.facebook.com/groups/3739709416119511/permalink/3739763179447468/
+// Episode 2
+// https://m.facebook.com/groups/333825854567625/permalink/411405840142959/
+// Episode 3
+// https://m.facebook.com/groups/333825854567625/permalink/416875039596039/
+// Episode 4
+// https://m.facebook.com/groups/333825854567625/permalink/356654368951440/
+// Episode 5
+// https://m.facebook.com/groups/333825854567625/permalink/362370048379872/
+// Episode 6
+// https://www.facebook.com/groups/333825854567625/permalink/368206891129521/
+// Episode 7
+// https://m.facebook.com/story.php?story_fbid=164484875400902&id=111415547374502
+// Episode 8
+// https://www.facebook.com/groups/333825854567625/permalink/379108086706068/
+// Episode 9
+// https://m.facebook.com/groups/333825854567625/permalink/384089759541234/
+// Episode 10
+// https://m.facebook.com/groups/333825854567625/permalink/388348012448742/
+// Episode 11
+// https://m.facebook.com/groups/333825854567625/permalink/392956688654541/
+// Episode 12
+// https://m.facebook.com/groups/333825854567625/permalink/397335084883368/
+// Episode 13
+// https://m.facebook.com/groups/333825854567625/permalink/402143177735892/
+// Episode 14
+// https://m.facebook.com/groups/333825854567625/permalink/406103514006525/
+// Episode 15
+// https://m.facebook.com/groups/333825854567625/permalink/410223516927858/
+// Episode 16
+// https://m.facebook.com/groups/333825854567625/permalink/414223979861145/
+// Episode 17
+// https://m.facebook.com/groups/333825854567625/permalink/418081196142090/
+// Episode 18
+// https://m.facebook.com/groups/333825854567625/permalink/421883822428494/
+// Episode 19
+// https://m.facebook.com/groups/333825854567625/permalink/425761148707428/
+// Episode 20
+// https://www.facebook.com/groups/333825854567625/permalink/429512551665621/
+// Episode 21
+// https://www.facebook.com/groups/333825854567625/permalink/433592257924317/
+// Episode 22
+// https://www.facebook.com/groups/333825854567625/permalink/437507927532750/
+// Episode 23
+// https://www.facebook.com/groups/333825854567625/permalink/441413187142224/
+// Episode 24
+// https://www.facebook.com/100267575499867/videos/130832372253162/
+// Episode 25
+// https://www.facebook.com/groups/333825854567625/permalink/449432776340265/
+// Episode 26
+// https://www.facebook.com/groups/333825854567625/permalink/457480418868834/
+// Episode 27
+// https://www.facebook.com/groups/333825854567625/permalink/461455295138013/
